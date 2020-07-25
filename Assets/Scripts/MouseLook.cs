4876c2079a66816d820455fb79b24b9046e22312
Eric Bernard <eric99b@gmail.com>
2020-07-25 19:04:36 -0400

    Add MouseLook and PlayerMovement scripts

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+	public float mouseSensitivity = 100f;
+	private float verticalRot = 0f;
+	public Transform playerBody;
+
+	public void Start() {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	public void Update() {
+
+		// Unlock the mouse cursor if "F" is pressed
+		if (Input.GetButtonDown("Free Camera")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+		
+		// Only move the character's view if the cursor is locked in
+		if (Cursor.lockState == CursorLockMode.Locked) {
+			float mx = Input.GetAxis("Mouse X");
+			float my = Input.GetAxis("Mouse Y");
+
+			mx *= mouseSensitivity * Time.deltaTime;
+			my *= mouseSensitivity * Time.deltaTime;
+
+			verticalRot -= my;
+			verticalRot = Mathf.Clamp(verticalRot, -90f, 90f);
+
+			transform.localRotation = Quaternion.Euler(verticalRot, 0f, 0f);
+			playerBody.Rotate(Vector3.up * mx);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public CharacterController controller;
+	public float movementSpeed = 12f;
+
+	public void Update() {
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Vector3 delta = transform.right * x + transform.forward * z;
+		controller.Move(delta * movementSpeed * Time.deltaTime);
+	}
+}
