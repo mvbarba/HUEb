79c71a34fed0d3660a7480a4e48f1183f6227e29
mvbarba <61198125+mvbarba@users.noreply.github.com>
2020-07-25 19:53:43 -0400

    Added LevelManager and organization

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LevelManager : MonoBehaviour
+{
+    public enum LevelNum
+    {
+        Level1,
+        Level2,
+        Level3,
+        Level4
+    }
+
+    [System.Serializable]
+    public struct LevelContainer
+    {
+        public GameObject levelPrefab;
+        public LevelNum levelNum;
+    }
+
+    [SerializeField]
+    public LevelContainer[] levels;
+
+    private static LevelManager instance;
+
+    // Where to instantiate levels
+    public Transform levelParent;
+
+
+    private List<GameObject> instantiatedLevels;
+
+    public static LevelManager Instance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void StartLevel(LevelNum levelNum)
+    {
+        foreach (LevelContainer level in levels)
+        {
+            if (level.levelNum == levelNum)
+            {
+                Instantiate(level.levelPrefab, levelParent);
+            }
+        }
+    }
+
+    public void ClearLevels()
+    {
+        foreach (GameObject child in levelParent.transform)
+        {
+            Destroy(child);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
