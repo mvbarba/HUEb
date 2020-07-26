912f24b77da58efee32a48222e336030b9d1f268
Eric Bernard <eric99b@gmail.com>
2020-07-25 23:26:21 -0400

    Remove vestigial code in Interactable.cs

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -8,18 +8,4 @@ public abstract class Interactable : MonoBehaviour
     public Constants.Color color;
 
     public abstract void OnInteract();
-
-
-    /* In update, check if our raycast is hitting this object
-     * 
-     * If we press interact button, run OnInteract()
-     */
-
-    private void Update()
-    {
-        if (false)
-        {
-            OnInteract();
-        }
-    }
 }
