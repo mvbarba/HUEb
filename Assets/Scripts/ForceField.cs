1f9df1befeff44f8bc3a7d31b41482236f0c6231
Eric Bernard <eric99b@gmail.com>
2020-07-28 10:53:36 -0400

    Begin elevator revamp

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+    private PlayerStateManager playerState;
+
+    private void Start() {
+        playerState = PlayerStateManager.Instance();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Trigger entry");
+        if (other.gameObject == playerState.gameObject) {
+            Debug.Log("player is here");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        
+    }
+}
