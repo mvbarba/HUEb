c987b74e7e7643b4d0611bf4b87e06d6bec96d62
mvbarba <61198125+mvbarba@users.noreply.github.com>
2020-07-28 18:56:39 -0400

    Forcefield and disolve shader

--- Assets/Scripts/Forcefield.cs
+++ Assets/Scripts/Forcefield.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Forcefield : MonoBehaviour
 {
+    private void Start()
+    {
+        Physics.IgnoreCollision(this.GetComponent<Collider>(), Camera.main.GetComponentInParent<CharacterController>());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        Debug.Log(other.name);
+        if (other.gameObject.tag == "CameraCenter")
             DimensionManager.Instance().EnterForcefield();
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "CameraCenter")
             DimensionManager.Instance().ExitForcefield();
     }
 }
