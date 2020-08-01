65ffa935fa1eebbaefd0468ff6f21265ce9c6aed
Eric Bernard <eric99b@gmail.com>
2020-08-01 18:13:12 -0400

    Add object 'net' to catch objects thrown out of the elevator

---
+++ Assets/Scripts/ObjectNet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectNet : MonoBehaviour
+{
+	public void OnCollisionEnter(Collision collision) {
+		Debug.Log("Found naughty boi");
+		collision.gameObject.transform.position = new Vector3(0, 13, 0);
+	}
+}
